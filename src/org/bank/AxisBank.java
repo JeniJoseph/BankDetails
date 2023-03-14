7a49f8c0b9123aeccaf51bfedeb90ae8bf84789e
JeniJoseph <jenij257@gmail.com>
2023-03-14 13:19:37 +0530

    Adding Details

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,25 @@
+package org.bank;
+
+public class AxisBank extends BankInfo{
+//	@Override
+//	
+//	public void deposit() {
+//		System.out.println("Deposit : 15000");
+//
+//	}
+//public static void main(String[] args) {
+//	AxisBank c = new AxisBank();
+//	c.deposit();
+//	c.fixed();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class BankInfo {
+	private void savings() {
+		System.out.println("Savings : 15000");
+
+	}
+	public void fixed() {
+		System.out.println("Fixed : 100000");
+	}
+	public void deposit() {
+	System.out.println("Deposit : 25000");
+
+	}
+
+}
